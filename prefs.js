fdb6ca46ca217d75d800468d4e2661601cecb400
ArvindParekh <aruparekh@gmail.com>
2026-02-01 11:50:30 -0500

    Fix GNOME Review issues: remove try-catch, fix prefs instance, bump v14

--- prefs.js
+++ prefs.js
@@ -25,7 +25,6 @@ export default class StopwatchPreferences extends ExtensionPreferences {
         });
         
         // Bind the switch to the settings
-        // Bind the switch to the settings
         window._settings = this.getSettings();
         window._settings.bind(
             'persist-timer',
